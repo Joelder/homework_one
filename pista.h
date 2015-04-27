9ff2f227a6a5864827c35bbc50a2105f9d63fdff
root <root@home-MS-7788.(none)>
2015-04-26 21:55:50 -0300

    First commit

---
+++ pista.h
@@ -0,0 +1,17 @@
+/*
+ * pista.h
+ *
+ *  Created on: Apr 26, 2015
+ *      Author: home
+ */
+
+#ifndef PISTA_H_
+#define PISTA_H_
+
+class pista {
+public:
+	pista();
+	virtual ~pista();
+};
+
+#endif /* PISTA_H_ */

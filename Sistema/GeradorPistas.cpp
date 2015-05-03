e7b26e6b1ed1aae170a2a04e983dee7b842a1af2
ThomasFFeijoo <thomwe8@hotmail.com>
2015-05-03 19:06:35 -0300

    Inicio do gerador de pistas

--- Sistema/GeradorPistas.cpp
+++ Sistema/GeradorPistas.cpp
@@ -6,25 +6,126 @@
  */
 
 #include "GeradorPistas.h"
-#include "../Pista/Pista.h"
+
+#include <stdlib.h>
+
+#include "../Pista/Sumidouro.h"
 
 GeradorPistas::GeradorPistas() {
-	Pista* pistaO1Leste;
-	Pista* pistaO1Oeste;
-	Pista* pistaN1Norte;
-	Pista* pistaN1Sul;
-	Pista* pistaS1;
-	Pista* pista;
-	Pista* pista;
-	Pista* pista;
-	Pista* pista;
-	Pista* pista;
-	Pista* pista;
-
+	this->pistaO1Oeste = NULL;
+	this->pistaN1Norte = NULL;
+	this->pistaS1Sul = NULL;
+	this->pistaN2Norte = NULL;
+	this->pistaL1Leste = NULL;
+	this->pistaS2Sul = NULL;
+	//Centrais
+	this->pistaC1Oeste = NULL;
+	this->pistaC1Leste = NULL;
+	//Fontes
+	this->pistaO1Leste = NULL;
+	this->pistaN1Sul = NULL;
+	this->pistaS1Norte = NULL;
+	this->pistaN2Sul = NULL;
+	this->pistaS2Norte = NULL;
+	this->pistaL1Oeste = NULL;
 
 }
 
 GeradorPistas::~GeradorPistas() {
 	// TODO Auto-generated destructor stub
+}
+
+//Sumidouros
+Pista* GeradorPistas::gerapistaO1Oeste() {
+	if (this->pistaO1Oeste != NULL) {
+		this->pistaO1Oeste = new Sumidouro(2000, 80, true);
+	}
+	return this->pistaO1Oeste;
+}
+
+Pista* GeradorPistas::gerapistaN1Norte() {
+	if (this->pistaN1Norte != NULL) {
+		this->pistaN1Norte = new Sumidouro(500, 60, true);
+	}
+	return this->pistaN1Norte;
+}
+
+Pista* GeradorPistas::gerapistaS1Sul() {
+	if (this->pistaS1Sul != NULL) {
+		this->pistaS1Sul = new Sumidouro(500, 60, true);
+	}
+	return this->pistaS1Sul;
+}
+
+Pista* GeradorPistas::gerapistaN2Norte() {
+	if (this->) {
+	this->
+}
+return this->
+}
+
+Pista* GeradorPistas::gerapistaL1Leste() {
+if (this->) {
+this->
+}
+return this->
+}
+
+Pista* GeradorPistas::gerapistaS2Sul() {
+if (this->) {
+this->
+}
+return this->
+}
+
+//Centrais
+Pista* GeradorPistas::gerapistaC1Oeste() {
+if (this->) {
+this->
+}
+return this->
+}
+Pista* GeradorPistas::gerapistaC1Leste() {
+if (this->) {
+this->
+}
+return this->
+}
+//Fontes
+Pista* GeradorPistas::gerapistaO1Leste() {
+if (this->) {
+this->
+}
+return this->
+}
+Pista* GeradorPistas::gerapistaN1Sul() {
+if (this->) {
+this->
+}
+return this->
+}
+Pista* GeradorPistas::gerapistaS1Norte() {
+if (this->) {
+this->
+}
+return this->
+}
+Pista* GeradorPistas::gerapistaN2Sul() {
+if (this->) {
+this->
+}
+return this->
+}
+Pista* GeradorPistas::gerapistaS2Norte() {
+if (this->) {
+this->
+}
+return this->
+}
+Pista* GeradorPistas::gerapistaL1Oeste() {
+if (this->) {
+this->
+}
+return this->
 }
 
--- Sistema/GeradorPistas.h
+++ Sistema/GeradorPistas.h
@@ -7,22 +7,50 @@
 
 #ifndef SISTEMA_GERADORPISTAS_H_
 #define SISTEMA_GERADORPISTAS_H_
+class Pista;
 
 class GeradorPistas {
+private:
+	//Sumidouros
+	Pista* pistaO1Oeste;
+	Pista* pistaN1Norte;
+	Pista* pistaS1Sul;
+	Pista* pistaN2Norte;
+	Pista* pistaL1Leste;
+	Pista* pistaS2Sul;
+	//Centrais
+	Pista* pistaC1Oeste;
+	Pista* pistaC1Leste;
+	//Fontes
 	Pista* pistaO1Leste;
-		Pista* pistaO1Oeste;
-		Pista* pistaN1Norte;
-		Pista* pistaN1Sul;
-		Pista* pistaS1;
-		Pista* pista;
-		Pista* pista;
-		Pista* pista;
-		Pista* pista;
-		Pista* pista;
-		Pista* pista;
+	Pista* pistaN1Sul;
+	Pista* pistaS1Norte;
+	Pista* pistaN2Sul;
+	Pista* pistaS2Norte;
+	Pista* pistaL1Oeste;
+
+	//Sumidouros
+		Pista* gerapistaO1Oeste();
+		Pista* gerapistaN1Norte();
+		Pista* gerapistaS1Sul();
+		Pista* gerapistaN2Norte();
+		Pista* gerapistaL1Leste();
+		Pista* gerapistaS2Sul();
+		//Centrais
+		Pista* gerapistaC1Oeste();
+		Pista* gerapistaC1Leste();
+		//Fontes
+		Pista* gerapistaO1Leste();
+		Pista* gerapistaN1Sul();
+		Pista* gerapistaS1Norte();
+		Pista* gerapistaN2Sul();
+		Pista* gerapistaS2Norte();
+		Pista* gerapistaL1Oeste();
+
 public:
 	GeradorPistas();
 	virtual ~GeradorPistas();
+	void geraPistas();
 };
 
 #endif /* SISTEMA_GERADORPISTAS_H_ */
